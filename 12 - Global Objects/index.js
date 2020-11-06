fd29b36db3be79c1c0f11000038ad6fa4538ec3f
Ayrton Souza <ayrtonsouza@icloud.com>
2020-11-06 14:16:19 -0300

    global objects

---
+++ 12 - Global Objects/index.js
@@ -0,0 +1,7 @@
+console.log('keys: ', Object.keys({ name: 'Ayrton' }))
+console.log('values: ', Object.values({ name: 'Ayrton' }))
+console.log('now: ', Date.now())
+console.log('random: ', Math.random())
+
+// Global (Node)
+console.log('global: ', global)
